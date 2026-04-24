ca21c1a9186500b4689a662a5b04cf1f83b11718
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-04-24 17:19:54 +0100

    Learned about comments in Java

---
+++ src/Comments.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+    public  static void main() {
+        //  This is a comment
+        //  Comments are usefull to understand your code
+        //    Two types of comments: Single line and multi line comments
+    /*
+        This is a multi line comment, which is also usefull to document your code. 
+    */
+        System.out.println("Hello Tizih Marko");
+    }
