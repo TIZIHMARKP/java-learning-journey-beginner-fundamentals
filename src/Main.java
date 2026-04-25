670f5491a0934cf08cc35df85bbb5ed8fdf76409
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-04-25 11:37:58 +0100

    Learend about reserved keywords in java

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,7 @@
-import java.time.LocalDateTime;
-import java.time.LocalDate;
-import java.util.Date;
-
 
 
 public  static void main() {
-    //   Reserved KEYWORD
-
-
+    //   ARITHMETIC OPERATIONS
 
 }
 
---
+++ src/_5_reservedKeyword.java
@@ -0,0 +1,15 @@
+
+
+public  static void main() {
+    //   Reserved KEYWORD
+//  They are works which are used by the language itself e.g public, class, static, void etc
+
+//    int public = 0; // it does not work
+//    int class = 12; // It does not work
+    int main = 0;   // It works
+
+//    Reserved keywords can't be use to name your classes, methods and so on;
+}
+
+
+
