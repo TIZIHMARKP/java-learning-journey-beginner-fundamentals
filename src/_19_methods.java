2b98e5b795748624f6d2fe35f6b6849866c26cb0
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-04-29 17:42:44 +0100

    Learned about methods in Java

--- src/_19_methods.java
+++ src/_19_methods.java
@@ -1,13 +1,16 @@
 
 
 public  static void main() {
-    //  Understanding  Methods
+    //   Methods
 //  Methods is a block of code which only runs when it is called. We can pass data into a method
 //  We have two types of methods:
 //    1. Built in methods: These are methods provided by the Java Run time environments
 //    2. Predefined Methods: These are methods which we build our selfs
 
-//     Continue at 03:00
+    System.out.println("Hello");
+    String brand = "Samsung";
+    System.out.println(brand.toUpperCase());   // println() and toUpperCase() are just some examples of methods
+    System.out.println(brand.endsWith("g"));  // Outputs: true
 
 
 }
