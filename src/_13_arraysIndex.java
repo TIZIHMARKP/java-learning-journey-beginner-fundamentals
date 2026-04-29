10cd7dc38e1cd217aaf0a56564733ac101affa3e
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-04-29 16:01:51 +0100

    Learned arrays indexing in Java

---
+++ src/_13_arraysIndex.java
@@ -0,0 +1,22 @@
+
+
+public  static void main() {
+    //   ARRAYS AND INDEXES
+//  Arrays allows us to create more than one variable that holds more than one value
+
+    int [] numbers = {2, 0, 1, 4, 100, 500};
+    int oneHundred = numbers[4];
+    int four = numbers[5];
+    int last = numbers[numbers.length - 1];
+
+    System.out.println(oneHundred);
+    System.out.println(four);
+    System.out.println(last);
+
+
+
+
+}
+
+
+
