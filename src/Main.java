fa0d523b4b42c203e2797910533765bc6898d7e9
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-04-29 17:16:46 +0100

    Learned the do while loop in java and practiced

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,8 @@
 
 
 public  static void main() {
-    //   While Loop
-//     It allows us to loop while a boolean expression evaluates to true
-
-    int count = 5;
-    while( count <= 4){
-        System.out.println("count " + count);   // Once the boolean expresion is false, it breaks out of the loop
-        count++;
-    }
-
+    //   SCANNER CLASS
+//
 
 
 }
--- src/_17_whileLoop.java
+++ src/_17_whileLoop.java
@@ -10,9 +10,26 @@ public  static void main() {
 //        count++;
 //    }
 
-//    ==== Do While Loop
+//    int count = 0;
+//    while( count <= 10){
+//        System.out.println("count " + count);   // count runs since conditions evaluates to true
+//        count++;
+//    }
 
+//    ==== Do While Loop  ===
+//    It runs before it checks the condition
 
+//    int count = 21;
+//    do{
+//        System.out.println(count);
+//        count++;
+//    } while (count <= 20);   // Code gets executed at least once before checking the condition
+
+    int count = 0;
+    do{
+        System.out.println(count);
+        count++;
+    } while (count <= 20);  // program runs till the conditions fail
 }
 
 
