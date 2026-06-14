b43c39788c56ed49e95dadc77b9bf9aab89fe21c
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-06-14 22:50:10 +0100

    Successfully writen a java program to print odd numbers between 1 - 100

--- src/javaBasicPrograms/OddNumber.java
+++ src/javaBasicPrograms/OddNumber.java
@@ -1,4 +1,16 @@
 package javaBasicPrograms;
 
+// Displaying Odd Numbers between 1 - 100
+
 public class OddNumber {
+
+    public static  void main(String args[]){
+        System.out.println("The Odd Numbers are: ");
+
+        for(int i = 1; i <= 100; i++){
+            if(i % 2 != 0){
+                System.out.println(i + " ");
+            }
+        }
+    }
 }
