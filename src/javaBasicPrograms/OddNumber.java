6c194e6df14315d26d3f11435200e4f264cd87ca
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-06-14 23:05:33 +0100

    Updated the code to let user input the upper bound of number to get odd numbers between 1 - upperBound of user

--- src/javaBasicPrograms/OddNumber.java
+++ src/javaBasicPrograms/OddNumber.java
@@ -2,12 +2,21 @@ package javaBasicPrograms;
 
 // Displaying Odd Numbers between 1 - 100
 
+import java.util.Scanner;
+
 public class OddNumber {
 
     public static  void main(String args[]){
+        int i;
+        Scanner scanner = new Scanner(System.in);
+
+        // User inputs the upper bound of number to get odd numbers from 1 - upperBount
+        System.out.println("Enter the maximum number you want to get odd numbers between: ");
+        int upper = scanner.nextInt();
         System.out.println("The Odd Numbers are: ");
 
-        for(int i = 1; i <= 100; i++){
+
+        for(i = 1; i <= upper; i++){
             // Checking if number is odd number
             if(i % 2 != 0){
                 System.out.println(i + " ");
