2bdf52fe77ac8a9c20e7a3fded3add050cff1091
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-06-19 23:33:05 +0100

    Optimized the program to let users input the upperBound of the number they will like to get sum of Odd Numbers up to

--- src/javaBasicPrograms/sumOfOddNumbers.java
+++ src/javaBasicPrograms/sumOfOddNumbers.java
@@ -1,17 +1,25 @@
 package javaBasicPrograms;
 
+// === Sum of odd numbers between 1 -100 ===
+
+import java.util.Scanner;
+
 public class sumOfOddNumbers {
 
     public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
         int sum = 0;
+        System.out.println("Enter the upper bound to get sum of Odd Numbers: ");
+        int upper = scanner.nextInt();
+        System.out.println("The sum of odd numbers between 1 and " + upper + " are: ");
 
-        for(int i = 1; i <= 100; i++){
+        for(int i = 1; i <= upper; i++){
             if(i % 2 != 0){
                 sum = sum + i;
             }
         }
 
-        System.out.println("The Sum of 100 Odd numbers are: " + sum);
+        System.out.println("Sum: " + sum);
     }
 
 }
