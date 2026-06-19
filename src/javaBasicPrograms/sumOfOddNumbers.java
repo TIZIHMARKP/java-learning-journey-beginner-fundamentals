63499a58806f15016d4ee691b5e3e56291acbbbd
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-06-19 23:23:56 +0100

    Written a basic java program to calculate sum of odd Numbers from 1 to 100

--- src/javaBasicPrograms/sumOfOddNumbers.java
+++ src/javaBasicPrograms/sumOfOddNumbers.java
@@ -1,4 +1,17 @@
 package javaBasicPrograms;
 
 public class sumOfOddNumbers {
+
+    public static void main(String[] args){
+        int sum = 0;
+
+        for(int i = 1; i <= 100; i++){
+            if(i % 2 != 0){
+                sum = sum + i;
+            }
+        }
+
+        System.out.println("The Sum of 100 Odd numbers are: " + sum);
+    }
+
 }
