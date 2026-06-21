788d0e1e739c45b50be73b60d5fb2c01f41a7ff9
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-06-21 23:38:03 +0100

    Successfully writen java program to calculate sum of first n numbers

--- src/javaBasicPrograms/sumOfN_Numbers.java
+++ src/javaBasicPrograms/sumOfN_Numbers.java
@@ -1,5 +1,20 @@
 package javaBasicPrograms;
 
-public class sumOfN_Numbers {
+import java.util.Scanner;
 
+public class sumOfN_Numbers {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int sum = 0;
+
+        System.out.println("Enter the range of numbers to find the sum: ");
+        int n = scanner.nextInt();
+
+        for(int i = 1; i <= n; i++){
+            sum = sum + i;
+        }
+        System.out.println("The sum of "+n+ " Numbers are: " + sum);
+
+
+    }
 }
