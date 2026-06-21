22fc001f29c52123906f567eb4492650891a3c46
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-06-21 23:59:31 +0100

    Added comments for explanations

--- src/javaBasicPrograms/sumOfDigitsOfaNumber.java
+++ src/javaBasicPrograms/sumOfDigitsOfaNumber.java
@@ -7,10 +7,16 @@ public class sumOfDigitsOfaNumber {
         Scanner scanner = new Scanner(System.in);
         int num, rem = 0, sum = 0, temp;
 
+        // rem stores the remainder (last digit when divided by 10)
+        // temp stores a copy of the original number for display purposes
+        // sum accumulates the sum of all digits
+
 
         System.out.println("Enter number to get sum of digits in number: ");
         num = scanner.nextInt();
         temp = num;
+
+
 
         while(num > 0){
             rem = num % 10;
