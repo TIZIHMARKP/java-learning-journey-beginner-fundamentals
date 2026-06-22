64185cb39ca12ec952a655a5cd93d0362aee5aca
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-06-22 13:05:56 +0100

    Used ternary operator for consie code to calculate electric bill

--- src/javaBasicPrograms/electricityBill.java
+++ src/javaBasicPrograms/electricityBill.java
@@ -2,20 +2,35 @@ package javaBasicPrograms;
 
 public class electricityBill {
     public static void main(String[] args){
-        int units = 123;
-        int bill = 0;
+//        int units = 123;
+//        int bill = 0;
 
-        if(units > 300){
-            bill = units * 8;
-        } else if(units >= 200) {
-            bill = units * 7;
-        } else if(units >= 100){
-            bill = units * 6;
-        } else {
-            bill = units * 5;
-        }
+        // === 1. USING IF ELSE  ====
+//        if(units > 300){
+//            bill = units * 8;
+//        } else if(units >= 200) {
+//            bill = units * 7;
+//        } else if(units >= 100){
+//            bill = units * 6;
+//        } else {
+//            bill = units * 5;
+//        }
+//        System.out.println("SOCADEL ELECTRICITY BILL");
+//        System.out.println("Units Consumed : " + units);
+//        System.out.println("Total Bill: " + bill);
+
+        // ==== 2. USING TERNARY OPERATOR =====
+        int units = 123;
+
+        int bill = (units > 300) ? units * 8 :
+                (units >= 200 ) ? units * 7 :
+                        (units >= 100) ? units * 6 :
+                                units * 5;
+
         System.out.println("SOCADEL ELECTRICITY BILL");
         System.out.println("Units Consumed : " + units);
         System.out.println("Total Bill: " + bill);
+
+
     }
 }
