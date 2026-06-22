6ef6deddc02b6793a429f485be276f65b4c31a10
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-06-22 12:56:49 +0100

    Successfully writen a basic java program to calculate eletric bill

--- src/javaBasicPrograms/electricityBill.java
+++ src/javaBasicPrograms/electricityBill.java
@@ -1,4 +1,21 @@
 package javaBasicPrograms;
 
 public class electricityBill {
+    public static void main(String[] args){
+        int units = 123;
+        int bill = 0;
+
+        if(units > 300){
+            bill = units * 8;
+        } else if(units >= 200) {
+            bill = units * 7;
+        } else if(units >= 100){
+            bill = units * 6;
+        } else {
+            bill = units * 5;
+        }
+        System.out.println("SOCADEL ELECTRICITY BILL");
+        System.out.println("Units Consumed : " + units);
+        System.out.println("Total Bill: " + bill);
+    }
 }
