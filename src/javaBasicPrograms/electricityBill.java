f3bd511309c19b7e23c6c97a5c9ca6e80716c069
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-06-22 13:18:36 +0100

    Writen a maintainable version of the code using array lookup

--- src/javaBasicPrograms/electricityBill.java
+++ src/javaBasicPrograms/electricityBill.java
@@ -20,17 +20,44 @@ public class electricityBill {
 //        System.out.println("Total Bill: " + bill);
 
         // ==== 2. USING TERNARY OPERATOR =====
+//        int units = 123;
+//
+//        int bill = (units > 300) ? units * 8 :
+//                (units >= 200 ) ? units * 7 :
+//                        (units >= 100) ? units * 6 :
+//                                units * 5;
+//
+//        System.out.println("SOCADEL ELECTRICITY BILL");
+//        System.out.println("Units Consumed : " + units);
+//        System.out.println("Total Bill: " + bill);
+
+        // ====== 3. USING ARRAY LOOKUP ======
         int units = 123;
 
-        int bill = (units > 300) ? units * 8 :
-                (units >= 200 ) ? units * 7 :
-                        (units >= 100) ? units * 6 :
-                                units * 5;
+        int [][] rateSlabs = {
+                {300, 8},
+                {200, 7},
+                {100, 6},
+                {0, 5}
+        };
+
+        int bill = calculateBill(units, rateSlabs);
 
         System.out.println("SOCADEL ELECTRICITY BILL");
         System.out.println("Units Consumed : " + units);
         System.out.println("Total Bill: " + bill);
 
-
     }
+
+    private static int calculateBill(int units, int[][] rateSlabs){
+        for(int[] slab : rateSlabs){
+            if(units > slab[0]){
+                return units * slab[1];
+            }
+        }
+        return units * 5;
+    }
+
+
 }
+
