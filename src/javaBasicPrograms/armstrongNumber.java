a7024e1c89205647285c076502342353ebf7db2c
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-06-23 22:12:30 +0100

    Added the explanation of what an armstrong number is

--- src/javaBasicPrograms/armstrongNumber.java
+++ src/javaBasicPrograms/armstrongNumber.java
@@ -1,5 +1,10 @@
 package javaBasicPrograms;
 
+// An Armstrong Number is a number that equals the sum of its own digits, each raised to the power of the number of digits.
+// It is also called Narcissistic numbers.
+// Example: 153, 9474 are Armstrong numbers
+// There are no 2-digit Armstrong numbers
+
 public class armstrongNumber {
     public static void main(String args[]){
         int n, arg, sum = 0, r;
