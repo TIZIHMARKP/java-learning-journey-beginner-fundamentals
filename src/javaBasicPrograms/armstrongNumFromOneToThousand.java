642501e30386a41f657ff54b8a5eb3313ca5e9dd
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-06-25 23:51:12 +0100

    Defined limit to find armstrong number

--- src/javaBasicPrograms/armstrongNumFromOneToThousand.java
+++ src/javaBasicPrograms/armstrongNumFromOneToThousand.java
@@ -1,4 +1,10 @@
 package javaBasicPrograms;
 
 public class armstrongNumFromOneToThousand {
+    public static void main(String[] args){
+        int num, rem, limit=1000, sum = 0;
+
+        System.out.println("Armstrong numbers from 1 to N: ");
+
+    }
 }
