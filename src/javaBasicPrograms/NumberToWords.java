91a0074a546ab2de763ae778d8e135a1445348a8
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-06-28 23:08:11 +0100

    Defined the string 1 to 19

--- src/javaBasicPrograms/NumberToWords.java
+++ src/javaBasicPrograms/NumberToWords.java
@@ -2,6 +2,19 @@ package javaBasicPrograms;
 
 public class NumberToWords {
 
+    public void pw(int n, String ch){
+        String one[] = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", " Eighteen", "Nineteen"};
+    }
 
+    public static void main(String[] args){
+        int n = 28;
+        System.out.print(n);
+        if(n <= 0){
+            System.out.println("Enter numbers greater than 0");
+        } else {
+            NumberToWords a = new NumberToWords();
+
+        }
+    }
 
 }
