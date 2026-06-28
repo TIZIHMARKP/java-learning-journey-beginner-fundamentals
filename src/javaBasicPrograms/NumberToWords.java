be8eb199b75379e94becb13bc0a9e03a29fd9c5e
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-06-28 23:31:08 +0100

    Little progress: Prints number output but not original number

--- src/javaBasicPrograms/NumberToWords.java
+++ src/javaBasicPrograms/NumberToWords.java
@@ -4,6 +4,22 @@ public class NumberToWords {
 
     public void pw(int n, String ch){
         String one[] = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", " Eighteen", "Nineteen"};
+
+        String ten[] = {"", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
+
+        if(n > 0){
+            if(n > 19){
+                System.out.print(ten[n/10] + " " + one[n % 10]);
+            } else {
+                System.out.println(one[n]);
+            }
+
+            if(!ch.isEmpty()){
+                System.out.println(" " + ch);
+            }
+        }
+
+
     }
 
     public static void main(String[] args){
@@ -13,7 +29,11 @@ public class NumberToWords {
             System.out.println("Enter numbers greater than 0");
         } else {
             NumberToWords a = new NumberToWords();
-
+            a.pw((n / 10000000) % 100, "crore");
+            a.pw((n / 100000) % 100, "lakh");
+            a.pw((n / 1000) % 100, "thousand");
+            a.pw((n / 100) % 10, "hundred");
+            a.pw((n % 100), "");
         }
     }
 
