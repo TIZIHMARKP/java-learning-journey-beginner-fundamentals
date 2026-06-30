e2eb0676bc651bb3bb68b027d1b0ba92d67f7628
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-06-30 23:52:37 +0100

    Updated the arrays for number words

--- src/javaBasicPrograms/NumberToWord2.java
+++ src/javaBasicPrograms/NumberToWord2.java
@@ -2,11 +2,18 @@ package javaBasicPrograms;
 
 public class NumberToWord2 {
 
-    String one[] = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", " Eighteen", "Nineteen"};
+    private static final String[] ONES = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", " Eighteen", "Nineteen"};
 
     // Array for tens: 20, 30, 40, .... 90
     // Index 0 is empty, index 1 is "Ten"
-    String ten[] = {"", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
+    private static final String[] TENS = {"", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
 
+    private static String convertToWords(int n){
+        StringBuilder result = new StringBuilder();
+
+        if(n >= 20){
+            result.append(TENS[n / 10 - 1]);
+        }
+    }
 
 }
