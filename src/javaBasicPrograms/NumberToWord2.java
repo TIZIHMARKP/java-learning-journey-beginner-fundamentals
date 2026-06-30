9805383c69aa7b78d12762149ce0fc3fd73ef81e
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-06-30 23:47:14 +0100

    Writen the Arrays of number words

--- src/javaBasicPrograms/NumberToWord2.java
+++ src/javaBasicPrograms/NumberToWord2.java
@@ -1,4 +1,12 @@
 package javaBasicPrograms;
 
 public class NumberToWord2 {
+
+    String one[] = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", " Eighteen", "Nineteen"};
+
+    // Array for tens: 20, 30, 40, .... 90
+    // Index 0 is empty, index 1 is "Ten"
+    String ten[] = {"", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
+
+
 }
