cbb05afcbf6091529c0a64ba71d82fcd05d4c68c
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-07-02 21:40:29 +0100

    Added the definition of what a palindrom number is

--- src/javaBasicPrograms/CheckingPalindromeNum.java
+++ src/javaBasicPrograms/CheckingPalindromeNum.java
@@ -2,6 +2,13 @@ package javaBasicPrograms;
 
 import java.util.Scanner;
 
+/**
+ * Program to check if a given number is a Palindrome or not.
+ * A palindrome number reads the same backward as forward.
+ * e.g: 121, 12321, 1001, 1221 are palindrome numbers
+ * e.g: 123, 456, 1000 are not palindrome numbers
+*/
+
 public class CheckingPalindromeNum {
     public static void main(String[] args){
         int n, pal, r, rev=0;
