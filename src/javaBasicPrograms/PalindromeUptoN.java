6c1ec5828642c16c629ef0ce469208564157b48c
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-07-03 20:49:13 +0100

    Successfully written a java program to check palindrome Number upto N

--- src/javaBasicPrograms/PalindromeUptoN.java
+++ src/javaBasicPrograms/PalindromeUptoN.java
@@ -1,4 +1,23 @@
 package javaBasicPrograms;
 
 public class PalindromeUptoN {
+    public static void main(String[] args){
+        int n, b, rev = 0;
+        int limit = 50;
+
+        System.out.println("Palindrome numbers from 1 to N: ");
+
+        for(int i = 1; i <= limit; i++){
+            n = i;
+            while (n > 0){
+                b = n % 10;
+                rev = rev * 10 + b;
+                n = n / 10;
+            }
+            if(rev == i){
+                System.out.println(i + " ");
+            }
+            rev = 0;
+        }
+    }
 }
