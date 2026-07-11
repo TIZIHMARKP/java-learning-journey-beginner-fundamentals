6874ac95e1ff27a67eec793c7d2b1023d5eb520f
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-07-11 23:02:20 +0100

    Added comments to code

--- src/javaBasicPrograms/PyramidPattern1.java
+++ src/javaBasicPrograms/PyramidPattern1.java
@@ -2,14 +2,28 @@ package javaBasicPrograms;
 
 public class PyramidPattern1 {
     public static void main(String[] args){
-        int n = 4, i, j;
-        for(i = 0; i < n; i++){
-//            System.out.println("\n");
-            for(j = 0; j <= i; j++){
+        int n = 7, i, j;
+
+        /**
+         * =====================================================
+         * PATTERN 1: LEFT-ALIGNED RIGHT TRIANGLE
+         * ===============================================
+         *
+         * *
+         * **
+         * ***
+         * ****
+         * *****
+         */
+        for(i = 0; i < n; i++){     // Outer Loop: Controls rows (o to n - 1)
+
+            for(j = 0; j <= i; j++){   // Inner Loop: Controls stars in each row
                 System.out.print("*");
             }
-            System.out.println("\n");
+            System.out.println();     // Moving to next line after each row
         }
+
+        System.out.println();
     }
 
 }
