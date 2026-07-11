430f5544d260f33d38f68a436631a2343b09a228
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-07-11 22:57:03 +0100

    Writen a java program to print right triangle (left aligned)

--- src/javaBasicPrograms/PyramidPattern1.java
+++ src/javaBasicPrograms/PyramidPattern1.java
@@ -1,4 +1,15 @@
 package javaBasicPrograms;
 
 public class PyramidPattern1 {
+    public static void main(String[] args){
+        int n = 4, i, j;
+        for(i = 0; i < n; i++){
+//            System.out.println("\n");
+            for(j = 0; j <= i; j++){
+                System.out.print("*");
+            }
+            System.out.println("\n");
+        }
+    }
+
 }
