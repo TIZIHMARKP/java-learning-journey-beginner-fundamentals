bdb2b62e7631a26c3492d6dcd196b86f91e0b17b
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-07-12 20:49:15 +0100

    Writen the code to display the lower half of the pyramid

--- src/javaBasicPrograms/PyramidPattern1.java
+++ src/javaBasicPrograms/PyramidPattern1.java
@@ -164,6 +164,16 @@ public class PyramidPattern1 {
             System.out.println();
         }
 
+        for(i = n - 2; i >= 0; i--){
+            for(j = 0; j < n - i - 1; j++){
+                System.out.println(" ");
+            }
+            for(j = 0; j < 2 * i + 1; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
 
 
 
