8b36ff269d584858052ecf4924d1d7054c290b31
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-07-12 20:46:35 +0100

    Writing the code to display the upper half pyramid

--- src/javaBasicPrograms/PyramidPattern1.java
+++ src/javaBasicPrograms/PyramidPattern1.java
@@ -152,6 +152,20 @@ public class PyramidPattern1 {
          *    *
          */
 
+        System.out.println("======= (Combination of pyramid + inverted pyramid) ======= ");
+
+        for(i = 0; i < n; i++){
+            for(j = 0; j < n - i - 1; j++){
+                System.out.print(" ");
+            }
+            for(j = 0; j < 2 * i + 1; j++){
+                System.out.println("*");
+            }
+            System.out.println();
+        }
+
+
+
 
 
 
