971561bfc0ad0b2efed12d256eae06d3550285d0
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-07-16 21:23:58 +0100

    Successfully printed pyramix of spaces

--- src/javaBasicPrograms/DifferentPatternPrograms1.java
+++ src/javaBasicPrograms/DifferentPatternPrograms1.java
@@ -1,16 +1,30 @@
 package javaBasicPrograms;
 
+import java.util.Scanner;
+
 public class DifferentPatternPrograms1 {
     public static void main(String[] args){
-        int i, j, k=8;
-        for(i = 0; i < 5; i++){
-            for(j = 0; j < k; j++){
+        int n, i, j, k, l, m, p, q, r, s;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the value of n: ");
+        n = scanner.nextInt();
+        p = n;
+        q = n;
+        for(i = n; i >= 1; i--){
+            for(j = 1; j <= i; j++){
+                System.out.print("*");
+            }
+            for(k = p*2; k < n*2-1; k++){
                 System.out.print(" ");
             }
-            k = k - 2;
-            for(j = 0; j <= i; j++){
-                System.out.print("* ");
+            for(l = i; l != 0; l--){
+                if(l == n){
+                    continue;
+                }
+                System.out.print("*");
             }
+            p--;
             System.out.println();
         }
     }
