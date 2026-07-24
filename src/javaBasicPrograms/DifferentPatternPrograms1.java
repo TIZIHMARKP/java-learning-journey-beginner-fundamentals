191d71a5476b9ae6d406db969a5492b2cdaf8de2
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-07-24 23:07:29 +0100

    Added comments explaining code

--- src/javaBasicPrograms/DifferentPatternPrograms1.java
+++ src/javaBasicPrograms/DifferentPatternPrograms1.java
@@ -111,9 +111,9 @@ public class DifferentPatternPrograms1 {
          * for n=5, output should be:
          * *       *   => Row 0: 1 star + 7 spaces + 1 star = 2 stars + 7 spaces
          * **     **   => Row 1: 2 stars + 5 spaces + 2 stars = 4 stars + 5 spaces
-         * ***   ***   => Row 2:
-         * **** ****   => Row 3:
-         * *********   => Row 4
+         * ***   ***   => Row 2: 3 stars + 3 spaces + 3 stars = 6 stars + 3 spaces
+         * **** ****   => Row 3: stars + 1 space  + 4 stars = 8 stars + 1 space
+         * *********   => Row 4: stars + 0 spaces + 4 stars = 9 stars total
          */
 
         System.out.println();
