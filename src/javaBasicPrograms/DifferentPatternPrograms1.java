5e80a604a198b5302bf1fef5a1cf3ec02a6d3efb
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-07-27 23:14:44 +0100

    Successfully printed lower half of hallow diamond

--- src/javaBasicPrograms/DifferentPatternPrograms1.java
+++ src/javaBasicPrograms/DifferentPatternPrograms1.java
@@ -297,8 +297,25 @@ public class DifferentPatternPrograms1 {
                 }
             }
             System.out.println();
+        }
 
+        //
+        for(i = n - 1; i >= 1; i--){
+            //
+            for(j = 1; j <= n - i; j++){
+                System.out.print(" ");
+            }
 
+            //
+            for(j = 1; j <= 2 * i - 1; j++){
+                //
+                if(j == 1 || j == 2 * i - 1){
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
         }
 
 
